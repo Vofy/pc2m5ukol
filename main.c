e7b30abe34ec2e7280b7cc00e403c62a6621cf04
Tomáš Batelka <tomas.batelka@email.cz>
2022-03-07 10:01:54 +0100

    Ošetřeno více mezer za sebou

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 int main()
 {
     char text[] = "cat dog hen duck goat pig horse rabbit sheep cow lion tiger wolf fox elephant cock hippo reindeer owl eagle sparrow swallow";
-    int wordLengthOccurency[16] = { 0 };
+
     int letterCount = 0;
+    int letterCountOccurency[16] = { 0 };
+
     int totalWords = 0;
 
     printf("List of words in string:\n\n");
@@ -13,12 +15,15 @@ int main()
     {
         if(text[i] == ' ' || text[i] == '\0')
         {
-            wordLengthOccurency[letterCount-1]++;
+            if (letterCount)
+            {
+                letterCountOccurency[letterCount-1]++;
 
-            for(int j = letterCount; j > 0; j--)
-                printf("%c", (j == letterCount) ? text[i-j]-32 : text[i-j]);
+                for(int j = letterCount; j > 0; j--)
+                    printf("%c", (j == letterCount) ? text[i-j]-32 : text[i-j]);
 
-            printf("\n");
+                printf("\n");
+            }
             letterCount = 0;
         }
         else
@@ -28,10 +33,11 @@ int main()
 
     for(int i = 0; i < 16; i++)
     {
-        if(wordLengthOccurency[i])
-            printf("The number of words with %d characters is %2d.\n", i + 1, wordLengthOccurency[i]);
-
-        totalWords += wordLengthOccurency[i];
+        if(letterCountOccurency[i])
+        {
+            printf("The number of words with %d characters is %2d.\n", i + 1, letterCountOccurency[i]);
+            totalWords += letterCountOccurency[i]; // Tohle by mohlo byt i mimo podminku, ale proc pricitat nulu kdyz uz mam podminku
+        }
     }
 
     printf("\nThe total number of words is %3d.\n", totalWords);
